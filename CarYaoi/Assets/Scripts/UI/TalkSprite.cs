9b46bd9ebcfaac4a0970868b33bc22c83d5c497f
Moss Limpert <70284036+MossLimpert@users.noreply.github.com>
2024-08-31 18:24:12 -0400

    visual novel intro scene DONE!

--- CarYaoi/Assets/Scripts/UI/TalkSprite.cs
+++ CarYaoi/Assets/Scripts/UI/TalkSprite.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class TalkSprite : MonoBehaviour
 {
     [SerializeField]
-    GameObject mechanic, climaxx, acura;
+    [Tooltip("You dont have to assign the mechanic if he's not in the scene")]
+    GameObject mechanic_neutral,mechanic_teehee, climaxx_neutral, climaxx_annoyed, climaxx_smug, acura_neutral, acura_happy;
 
     // Start is called before the first frame update
     void Start()
@@ -14,39 +15,95 @@ public class TalkSprite : MonoBehaviour
 
     }
 
-    // show Maxx's sprite
-    public void showMaxx()
+//
+// MAXX
+//
+    // show Maxx's Neutral sprite
+    public void showMaxxNeutral()
     {
-        climaxx.SetActive(true);
+        climaxx_neutral.SetActive(true);
     }
 
-    // hide Maxx's sprite
-    public void hideMaxx()
+    // hide Maxx's Neutral sprite
+    public void hideMaxxNeutral()
     {
-        climaxx.SetActive(false);
+        climaxx_neutral.SetActive(false);
     }
+
+    // show Maxx's Annoyed sprite
+    public void showMaxxAnnoyed()
+    {
+        climaxx_annoyed.SetActive(true);
+    }
+
+    // hide maxx's annoyed sprite
+    public void hideMaxxAnnoyed()
+    {
+        climaxx_annoyed.SetActive(false);
+    }
+
+    // show Maxx's smug sprite
+    public void showMaxxSmug() {
+        climaxx_smug.SetActive(true);
+    }
+
+    // hide Maxx's smug sprite
+    public void hideMaxxSmug(){
+        climaxx_smug.SetActive(false);
+    }
+
+    //
+    // ACURA
+    //
 
     // show Acura's sprite
-    public void showAcura()
+    public void showAcuraNeutral()
     {
-        acura.SetActive(true);
+        acura_neutral.SetActive(true);
     }
 
     // hide Acura's sprite
-    public void hideAcura()
+    public void hideAcuraNeutral()
     {
-        acura.SetActive(false);
+        acura_neutral.SetActive(false);
     }
 
-    // show Mechanic's sprite
-    public void showMechanic()
+    // show Acura happy sprite
+    public void showAcuraHappy()
     {
-        mechanic.SetActive(true);
+        acura_happy.SetActive(true);
+    }
+
+    // hide acura happy sprite
+    public void hideAcuraHappy() {
+        acura_happy.SetActive(false);
+    }
+
+
+    //
+    // MECHANIC
+    //
+
+    // show Mechanic's sprite
+    public void showMechanicNeutral()
+    {
+        mechanic_neutral.SetActive(true);
     }
 
     // hide Mechanic's sprite
-    public void hideMechanic()
+    public void hideMechanicNeutral()
     {
-        mechanic.SetActive(false);
+        mechanic_neutral.SetActive(false);
     }
+
+    // show mechanic tee hee sprite
+    public void showMechanicTeehee() {
+        mechanic_teehee.SetActive(true);
+    }
+
+    // hide mechanic tee hee sprite
+    public void hideMechanicTeehee() {
+        mechanic_teehee.SetActive(false);
+    }
+
 }
